ac7d0e5880e630a707785b5b9c8caaabb7919f1c
Santiago Linares <santiagolinares89@gmail.com>
2024-07-01 20:52:00 -0300

    .

--- TrabajoFinalPrueba1/PegasusWebV1/PegasusWeb/PegasusWeb/Pages/Materia/Materia.cshtml.cs
+++ TrabajoFinalPrueba1/PegasusWebV1/PegasusWeb/PegasusWeb/Pages/Materia/Materia.cshtml.cs
@@ -2,28 +2,27 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Newtonsoft.Json;
 using PegasusWeb.Entities;
-using System.Text;
 using System.Text.Json.Serialization;
 
-namespace PegasusWeb.Pages
+namespace PegasusWeb.Pages.Materia
 {
     public class MateriaModel : PageModel
     {
         static HttpClient client = new HttpClient();
-        private List<Entities.Materia> materias = new List<Entities.Materia>();
-
-        public List<PegasusWeb.Entities.Materia> Materias { get => materias; set => materias = value; }
+        public List<PegasusWeb.Entities.Materia> Materias { get; set; }
 
         public async Task OnGetAsync()
         {
-            materias = await GetMateriasAsync();
+            Materias = await GetMateriasAsync();
         }
 
-        public async Task<List<PegasusWeb.Entities.Materia>> GetMateriasAsync()
+        static async Task<List<PegasusWeb.Entities.Materia>> GetMateriasAsync()
         {
             List<PegasusWeb.Entities.Materia> getmaterias = new List<PegasusWeb.Entities.Materia>();
 
-            HttpResponseMessage response = await client.GetAsync("https://pegasus.azure-api.net/v1/Materia/GetMateriasForCombo");
+            //HttpResponseMessage response = await client.GetAsync("https://pegasus.azure-api.net/v1/Materia/GetMateriasForCombo");
+            HttpResponseMessage response = await client.GetAsync("https://localhost:7130/Materia/GetMateriasForCombo");
+
             if (response.IsSuccessStatusCode)
             {
                 string materiasJson = await response.Content.ReadAsStringAsync();
@@ -35,17 +34,5 @@ namespace PegasusWeb.Pages
 
             return getmaterias;
         }
-
-        public async Task<IActionResult> OnPost(int itemId)
-        {
-            HttpResponseMessage response = await client.GetAsync($"https://pegasus.azure-api.net/v1/Materia/DeleteMateria?id={itemId}");
-            if (!response.IsSuccessStatusCode)
-            {
-                this.ModelState.AddModelError("materia", "Hubo un error inesperado al eliminar la Materia");
-            }
-
-            return RedirectToPage();
-        }
-
     }
 }
